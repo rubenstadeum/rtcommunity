5360b4aa1b4b3c9a3650e17ab6a6240f65772be0
Rubens Tadeu <rubenstadeumoura@gmail.com>
2022-07-12 21:25:10 -0300

    Project created

---
+++ backend/src/main/java/com/rtm/rtcommunity/RtcommunityApplication.java
@@ -0,0 +1,13 @@
+package com.rtm.rtcommunity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RtcommunityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RtcommunityApplication.class, args);
+	}
+
+}
